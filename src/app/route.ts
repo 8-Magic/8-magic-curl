51ae6d2398aae0067e349c2eaee751422368b1d8
Ali Almasi <a710almasi@gmail.com>
2025-07-28 19:57:23 +0330

    change: add ua check

--- src/app/route.ts
+++ src/app/route.ts
@@ -5,60 +5,61 @@ import figures from "figures";
 import stripAnsi from "strip-ansi";
 
 export async function GET(req: NextRequest) {
-	try {
-		const fetchData = await fetch("https://api.8.alialmasi.ir/v2/answers");
-		const pureData = await fetchData.json();
-		const { answer }: { answer: string } = pureData?.data.answer;
+	const userAgent = (req.headers.get("user-agent") || "").toLowerCase();
+	if (!userAgent.includes("curl"))
+		return Response.redirect("https://api.8.alialmasi.ir", 302);
+	else
+		try {
+			const fetchData = await fetch("https://api.8.alialmasi.ir/v2/answers");
+			const pureData = await fetchData.json();
+			const { answer }: { answer: string } = pureData?.data.answer;
 
-		const output = boxen(
-			[
-				`${chalk.green.bold(figures.tick)} ${chalk.bold(
-					"Here's your answer!"
-				)}`,
-				"",
-				`${chalk.blue(figures.pointer)} ${chalk.cyan("Answer:")} ${chalk.yellow(
-					`${answer}`
-				)}`,
-				chalk.dim("Tip: Add ?json=1 or ?noColor=1 to tweak output.")
-			].join("\n"),
-			{
-				padding: 1,
-				borderStyle: "singleDouble",
-				borderColor: "green"
+			const output = boxen(
+				[
+					`${chalk.green.bold(figures.tick)} ${chalk.bold(
+						"Here's your answer!"
+					)}`,
+					"",
+					`${figures.triangleRight} ${chalk.greenBright(answer)}`
+				].join("\n"),
+				{
+					padding: 1,
+					borderStyle: "singleDouble",
+					borderColor: "green"
+				}
+			);
+
+			const { searchParams } = new URL(req.url);
+			const noColor =
+				searchParams.get("noColor") || searchParams.get("nocolor") === "1";
+			const returnJson =
+				searchParams.get("json") || searchParams.get("JSON") === "1";
+
+			if (returnJson) {
+				return new Response(JSON.stringify(pureData), {
+					status: 200,
+					headers: { "Content-Type": "application/json" }
+				});
 			}
-		);
 
-		const { searchParams } = new URL(req.url);
-		const noColor =
-			searchParams.get("noColor") || searchParams.get("nocolor") === "1";
-		const returnJson =
-			searchParams.get("json") || searchParams.get("JSON") === "1";
-
-		if (returnJson) {
-			return new Response(JSON.stringify(pureData), {
+			return new Response(noColor ? stripAnsi(output) : output, {
 				status: 200,
-				headers: { "Content-Type": "application/json" }
+				headers: { "Content-Type": "text/plain; charset=utf-8" }
+			});
+		} catch (err: unknown) {
+			const errorMsg = boxen(
+				`${figures.cross} Failed to fetch data.
+			${JSON.stringify(err, null, 2)}`,
+				{
+					padding: 1,
+					borderStyle: "classic",
+					borderColor: "red"
+				}
+			);
+
+			return new Response(stripAnsi(errorMsg), {
+				status: 500,
+				headers: { "Content-Type": "text/plain; charset=utf-8" }
 			});
 		}
-
-		return new Response(noColor ? stripAnsi(output) : output, {
-			status: 200,
-			headers: { "Content-Type": "text/plain; charset=utf-8" }
-		});
-	} catch (err: unknown) {
-		const errorMsg = boxen(
-			`${figures.cross} Failed to fetch data.
-			${JSON.stringify(err, null, 2)}`,
-			{
-				padding: 1,
-				borderStyle: "classic",
-				borderColor: "red"
-			}
-		);
-
-		return new Response(stripAnsi(errorMsg), {
-			status: 500,
-			headers: { "Content-Type": "text/plain; charset=utf-8" }
-		});
-	}
 }
