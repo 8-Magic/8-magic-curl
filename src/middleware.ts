5267de7c379ded3db9c03df69ea6a62bd8dd0de8
Ali Almasi <a710almasi@gmail.com>
2025-07-29 09:06:07 +0330

    remove: ua login from middleware, allowing any client

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-	const userAgent: string = (req.headers.get("user-agent") || "").toLowerCase();
-	console.warn("User Agent: " + userAgent);
-	const isCli = userAgent.includes("curl");
-	console.warn("Is curl: " + isCli ? "yes" : "no");
+export function middleware(req: NextRequest): NextResponse {
 	const pathname = req.nextUrl.pathname;
 
-	if (!isCli)
-		return new NextResponse("Redirecting to main API...", {
-			status: 302,
+	if (pathname !== "/")
+		return new NextResponse("404 Not Found\n", {
 			headers: {
-				Location: "https://api.8.alialmasi.ir",
 				"Content-Type": "text/plain; charset=utf-8"
 			}
 		});
-
-	if (pathname !== "/") return NextResponse.redirect(new URL("/", req.url));
 
 	return NextResponse.next();
 }
